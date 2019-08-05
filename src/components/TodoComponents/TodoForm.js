76fc52d6a16673030294d49345d2c6c190912b3d
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-05 20:49:29 +0100

    WIP: setup changeInputHandler for form state on ToDoForm component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,11 +3,28 @@ import React from 'react'
 class TodoForm extends React.Component {
     constructor() {
         super()
+        this.state = {
+            input: ''
+        }
     }
+
+    changeInputHandler = event => {
+        this.setState({
+            input: event.target.value
+        })
+    }
+
 
     render() {
         return (
-            <p>placeholder Todo component</p>
+            <>
+                <p>placeholder Todo component</p>
+                <form>
+                    <input onChange={this.changeInputHandler}/>
+                    <button>Add Todo</button>
+                    <button>Clear Completed</button>
+                </form>
+            </>
         )
     }
 }
