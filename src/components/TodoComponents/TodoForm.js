775cbdaed8c3710ff0783f6e987715eb75258713
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-05 21:21:27 +0100

    WIP: setup controlled input to be state, input now resets to empty string on form submit

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -17,6 +17,9 @@ class TodoForm extends React.Component {
     addTodoHandler = event => {
         event.preventDefault()
         this.props.addTodo(this.state.input)
+        this.setState({
+            input: ''
+        })
     }
 
 
@@ -25,7 +28,10 @@ class TodoForm extends React.Component {
             <>
                 <p>placeholder Todo component</p>
                 <form onSubmit={this.addTodoHandler}>
-                    <input onChange={this.changeInputHandler}/>
+                    <input 
+                        onChange={this.changeInputHandler}
+                        value={this.state.input}
+                    />
                     <button>Add Todo</button>
                     <button>Clear Completed</button>
                 </form>
