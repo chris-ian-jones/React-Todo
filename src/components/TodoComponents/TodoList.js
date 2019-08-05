109b2fb8f6ac51240ded81d62662509cf556a230
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-05 20:17:30 +0100

    WIP: setup scaffolding for and exported/imported TodoList.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+
+class TodoList extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <p>TodoList Componet setup</p>
+        )
+    }
+}
+
+export default TodoList
